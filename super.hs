5dde23ce9afb793609f200a5ad9359ea0080f79d
jeffersoncarpenter <jeffersoncarpenter2@gmail.com>
2013-09-24 15:38:43 -0500

    Removed some cruft code and re-documented some stuff.
    A great breakthrough was made today.  The main function can now be simple
    like I wanted it to be.
    
    However, there are still some usability problems.
    
    First of all, the type class takes four parameters.  I think that's too
    much.  The OutputEmulator type is actually known from the Input,
    InputEmulator, and Output types.  So it should be possible to make
    OutputEmulator not be a parameter.
    
    Ideally, the CanBe type class should only have two parameters: input and
    inputEmulator.  I don't know if that's possible, though.
    
    Second problem: you have to write the type signature of the main function
    to get it to compile.  This is a usability annoyance that needs to be
    fixed.

--- super.hs
+++ super.hs
@@ -7,39 +7,35 @@ import Data.Traversable
 
 
 
---main = (asDefault $ as putStrLn "aoeu") :: IO ()
+--main = (as putStrLn "hello world") :: IO ()
 main = as putStrLn ["aoeu", "htns"] :: IO [()]
---main = (as (\x -> return x :: IO ()) $ (as putStrLn ["aoeu", "htns"] :: [IO ()])) :: IO [()]
---main = asDefault $ as putStrLn ["aoeu", "htns"]
 
 
--- The CanBe type class allows certain types to expose themselves as other types
--- The CanBe type class is a parametric type class taking the following types:
+
+
+-- The CanBe type class allows certain types to expose themselves to functions as other types, by taking control over how the function is executed
+
+-- input is the type that the function expects
+-- output is the type that the function returns
+-- inputEmulator is the type that we are enabling to expose itself as the input type
+-- outputEmulator is the type that is returned instead of output, whenever inputEmulator emulates input
 
 class CanBe input output outputEmulator inputEmulator where
   as :: (input -> output) -> inputEmulator -> outputEmulator
---  asDefault :: b -> OutputEmulator a b a
+
 
 -- any type can trivially emulate itself
 instance CanBe a c c a where
   as func = func
---  asDefault arg = arg
+
 
 -- a functor of a type can emulate that type by fmapping over itself
 instance (Functor f) => CanBe a c (f c) (f a) where
   as = fmap
---  asDefault arg = arg
 
 
+-- this one feels kind of contrived tbh, even though the implementation is dead simple
+-- a function that returns a monadic value, can operate on a collection of input values
+-- however, it must make sure to return a monad of the traversable, not a traversable of monads
 instance (Traversable t, Monad m) => CanBe a (m c) (m (t c)) (t a) where
   as = mapM
-
-
-
--- this one is merely a kludge so that I don't have to use "sequence" in main
--- any iterable structure full of monadic types should be able to map and bind over itself for you
---instance (Traversable t, Monad m) => CanBe (m a) (m c) (t (m a)) where
---  type OutputEmulator (m a) (m c) (t (m a)) = m (t c)
---  type DefaultOutputEmulator a (t (m a)) (m c) = m (t a)
---  as func arg = sequence $ map func arg
---  asDefault arg = sequence arg
