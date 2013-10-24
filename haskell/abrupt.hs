96310fad820b1c2e341b95055533918342537e2a
jeffersoncarpenter <jeffersoncarpenter2@gmail.com>
2013-10-24 08:46:17 -0500

    now you can putStrLn a getLine without calling >>= through the abrupt.hs framework

--- haskell/abrupt.hs
+++ haskell/abrupt.hs
@@ -1,31 +1,34 @@
 {-# LANGUAGE TypeSynonymInstances, KindSignatures, GADTs, TypeFamilies, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
 
+import Control.Monad
+
+
 main = do
-  -- ijoin $ imap putStrLn "hello world"                 -- the base case does not work!
-  ijoin $ imap putStrLn ["hello", "world"]               -- prints "hello" and then "world"
-  ijoin $ imap putStrLn []                               -- no-op
-  ijoin $ imap putStrLn (Just "foo bar")                 -- prints "foo bar"
-  ijoin $ imap putStrLn Nothing                          -- prints an empty line
-  ijoin $ imap (ijoin . (imap putStrLn)) [Just "hello"]  -- supports nesting types
-  ijoin $ imap putStrLn $                                
-          imap show $                                    -- supports composing functions using imap
-          imap length ["aoeu", "aoeuaoeu"]               -- none of these functions takes an array... until now
+  putStrLn "hello world"                                 -- 
+  ijoin $ fmap putStrLn ["hello", "world"]               -- prints "hello" and then "world"
+  ijoin $ fmap putStrLn []                               -- no-op
+  ijoin $ fmap putStrLn (Just "foo bar")                 -- prints "foo bar"
+  ijoin $ fmap putStrLn Nothing                          -- prints an empty line
+  ijoin $ fmap (ijoin . (fmap putStrLn)) [Just "hello"]  -- supports nesting types using fmap and ijoin additional times
+  ijoin $ fmap putStrLn $                                
+          fmap show $                                    -- supports composing functions using fmap
+          fmap length ["aoeu", "aoeuaoeu"]               -- none of these functions takes an array... until now
+  putStrLn "enter a string to be repeated back:"
+  ijoin $ fmap putStrLn getLine
 
 
 
-class Invisible f where
-  imap :: (t -> u) -> f t -> f u
-  ijoin :: (Monad m) => f (m t) -> m (f t)
+class Joinable m f where
+  ijoin :: f (m a) -> m (f a)
 
-
-instance Invisible [] where
-  imap = map
+instance (Monad m) => Joinable m [] where
   ijoin = sequence
 
-instance Invisible Maybe where
-  imap = fmap
+instance (Monad m) => Joinable m Maybe where
   ijoin Nothing = return Nothing
   ijoin (Just val) = val >>= \a -> return (Just a)
 
+instance (Monad m) => Joinable m m where
+  ijoin arg = liftM return $ join arg
 
 
