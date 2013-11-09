5d072ddec32462e5169999e1cf24c1cd57704764
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2013-11-09 20:51:07 +0000

    the internals are messier but it's slightly nicer to work with

--- haskell/abrupt.hs
+++ haskell/abrupt.hs
@@ -1,25 +1,90 @@
-{-# LANGUAGE TypeSynonymInstances, KindSignatures, GADTs, TypeFamilies, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances, KindSignatures, GADTs, TypeFamilies, MultiParamTypeClasses, FunctionalDependencies, FlexibleInstances, UndecidableInstances, FlexibleContexts, OverlappingInstances #-}
 
 import Control.Monad
 
 
-main = do
-  putStrLn "hello world"                                 -- 
-  ijoin $ fmap putStrLn ["hello", "world"]               -- prints "hello" and then "world"
-  ijoin $ fmap putStrLn []                               -- no-op
-  ijoin $ fmap putStrLn (Just "foo bar")                 -- prints "foo bar"
-  ijoin $ fmap putStrLn Nothing                          -- prints an empty line
-  ijoin $ fmap (ijoin . (fmap putStrLn)) [Just "hello"]  -- supports nesting types using fmap and ijoin additional times
-  ijoin $ fmap putStrLn $                                
-          fmap show $                                    -- supports composing functions using fmap
-          fmap length ["aoeu", "aoeuaoeu"]               -- none of these functions takes an array... until now
-  putStrLn "enter a string to be repeated back:"
-  ijoin $ fmap putStrLn getLine
 
+main = do
+  -- prints a string
+  sputStrLn "aoeu"
+
+  -- prints an array of strings
+  sputStrLn ["hello", "world"]
+
+  -- accepts an empty array (but you have to explicitly type it - eww)
+  sputStrLn ([] :: [String])
+
+  -- prints a nullable string
+  sputStrLn (Just "foo bar")
+
+  -- prints nothing if the nullable string is null (explicitly typed eww gross)
+  sputStrLn (Nothing :: Maybe String)
+
+  -- it happens twice when you pass in an array of nullable types
+  sputStrLn [Just "hello"]
+
+  -- this one doesn't work yet
+  sputStrLn $ fmap show $ fmap length ["aoeu", "aoeuaoeu"]
+
+  -- binds monads appropriately
+  sputStrLn "enter a string to be repeated back:"
+  sputStrLn getLine
+
+
+
+-- this code looks much cleaner than before, but it still has some problems:
+
+
+-- 1: "empty" functors (like [] and Nothing) need to be explicitly typed
+
+-- 2: sshow doesn't compile
+-- I think this is because show could have separate implementations for [t] and t
+-- CanBe's as function wouldn't know whether to use the [t] implementation or recursively apply the t implementation
+
+
+
+
+type family CBResult t e o where
+  CBResult t t o = o
+  CBResult t (f e) o = f (CBResult t e o)
+
+
+sputStrLn :: (CanBeM IO String e ()) => e -> IO (CBResult String e ())
+sputStrLn = asM putStrLn
+
+
+class CanBeM m t e o where
+  asM :: (t -> m o) -> e -> m (CBResult t e o)
+
+instance CanBeM m t t o where
+  asM func = func
+
+instance (Functor f, Joinable m f, f o ~ CBResult t (f t) o) => CanBeM m t (f t) o where
+  asM func = ijoin . fmap func
+
+instance (Functor f,
+          Joinable m f,
+          f o ~ CBResult t (f t) o,
+          Functor g,
+          Joinable m g,
+          g (f o) ~ CBResult t (g (f t)) o) => CanBeM m t (g (f t)) o where
+  asM func = ijoin . fmap (ijoin . fmap func)
+
+
+
+class CanBe t e o where
+  as :: (t -> o) -> e -> CBResult t e o
+
+instance CanBe t t o where
+  as func = func
+
+instance (CBResult t (f e) o ~ f (CBResult t e o), CanBe t e o, Functor f) => CanBe t (f e) o where
+  as func = fmap (as func)
 
 
 class Joinable m f where
   ijoin :: f (m a) -> m (f a)
+  imap :: (a -> b) -> f a -> f b
 
 instance (Monad m) => Joinable m [] where
   ijoin = sequence
